226b748eb6473881e5700facfcc8041b98e22528
TatyanaGolubitskaya <tatyanagolubitskaya3@gmail.com>
2024-07-11 16:00:21 +0300

    commit-001

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="flex justify-between border-b border-slate-200 px-10 py-8">
+    <div>
+      <h2 class="text-xl font-bold uppercase">VueS</h2>
+    </div>
+    <ul class="flex items-center gap-10">
+      <li>
+        <span>О нас</span>
+      </li>
+      <li>
+        <span>Контакты</span>
+      </li>
+      <li>
+        <span>Войти</span>
+      </li>
+    </ul>
+  </header>
+</template>
